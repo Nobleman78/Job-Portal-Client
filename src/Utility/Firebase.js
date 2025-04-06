2a6c9ccd16d81ab9bbc73bb1102e95c242ca09d5
Jesmin Chakma <jesminchakma39@gmail.com>
2025-04-06 18:04:22 +0600

    FirseBase Updated

--- src/Utility/Firebase.js
+++ src/Utility/Firebase.js
@@ -4,13 +4,14 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId
+  apiKey: "AIzaSyC4sqFZ2lJ9Uz97XYNf-TAzpAuQoEFKiS4",
+  authDomain: "job-portal-aa443.firebaseapp.com",
+  projectId: "job-portal-aa443",
+  storageBucket: "job-portal-aa443.firebasestorage.app",
+  messagingSenderId: "420094849978",
+  appId: "1:420094849978:web:479b3869f2c64c5d8aa383"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);

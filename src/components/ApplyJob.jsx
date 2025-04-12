2f69bb0fa336e5ff26b763703115abc21bc20f7a
Jesmin Chakma <jesminchakma39@gmail.com>
2025-04-12 15:53:09 +0600

    Form Validation added

--- src/components/ApplyJob.jsx
+++ src/components/ApplyJob.jsx
@@ -17,6 +17,42 @@ const ApplyJob = () => {
         }
 
     }
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        number: '',
+        workplaceType: '',
+        location: '',
+        type: '',
+        resume: ''
+    })
+    const verifyForm = () => {
+        return (
+            formData.name.trim() !== '' &&
+            formData.email.trim() !== '' &&
+            formData.number.trim() !== '' &&
+            formData.workplaceType.trim() !== '' &&
+            formData.location.trim() !== '' &&
+            formData.type.trim() !== '' &&
+            formData.resume.trim() !== ''
+        );
+    }
+    const handleInputChange = (e) => {
+        const { name, value, files } = e.target;
+
+        if (name === "resume") {
+            setFormData({
+                ...formData,
+                [name]: files[0]?.name || '', 
+            });
+        } else {
+            setFormData({
+                ...formData,
+                [name]: value,
+            });
+        }
+    };
+
     const submitJobApplication = (e) => {
         e.preventDefault();
         const form = e.target;
@@ -24,7 +60,7 @@ const ApplyJob = () => {
         const email = form.email.value;
         const phoneNumber = form.number.value;
         const location = form.location.value;
-        const resumeFile = form.resume.files[0]; // Get actual file object
+        const resumeFile = form.resume.files[0];
         const workPlaceType = form.workplaceType.value;
         const type = form.type.value;
         form.reset();
@@ -36,23 +72,23 @@ const ApplyJob = () => {
             <form onSubmit={submitJobApplication} className='shadow-xl w-2xl mx-auto px-10 py-10 rounded-2xl'>
                 <div className='flex flex-col mb-2 gap-2'>
                     <label htmlFor="">Full Name:</label>
-                    <input type="text" name='name' placeholder='Enter your name' className='border outline-none shadow-none w-full px-5 py-2 rounded-full' />
+                    <input onChange={handleInputChange} type="text" name='name' required value={formData.name} placeholder='Enter your name' className='border outline-none shadow-none w-full px-5 py-2 rounded-full' />
                 </div>
 
                 <div className='flex flex-col gap-2 mb-2'>
                     <label htmlFor="">Email:</label>
-                    <input type="email" name='email' placeholder='Enter your email' className='border w-full outline-none shadow-none px-5 py-2 rounded-full' />
+                    <input onChange={handleInputChange} type="email" name='email' required value={formData.email} placeholder='Enter your email' className='border w-full outline-none shadow-none px-5 py-2 rounded-full' />
                 </div>
 
                 <div className='flex flex-col gap-2 mb-2'>
                     <label htmlFor="">Phone Number:</label>
-                    <input type="number" name='number' placeholder='Enter your phone number' className='px-5 py-2 rounded-full w-full border outline-none shadow-none' />
+                    <input onChange={handleInputChange} type="number" name='number' required value={formData.number} placeholder='Enter your phone number' className='px-5 py-2 rounded-full w-full border outline-none shadow-none' />
                 </div>
 
                 <div className='flex justify-between gap-2'>
                     <div className='flex flex-col w-1/2 gap-2'>
                         <label>WorkPlace Type:</label>
-                        <select name="workplaceType" className='w-full border px-5 py-2 rounded-full outline-none shadow-none'>
+                        <select onChange={handleInputChange} name="workplaceType" required value={formData.workplaceType} className='w-full border px-5 py-2 rounded-full outline-none shadow-none'>
                             <option value="onsite">Onsite</option>
                             <option value="remote">Remote</option>
                         </select>
@@ -60,13 +96,13 @@ const ApplyJob = () => {
 
                     <div className='flex flex-col gap-2 w-1/2'>
                         <label>Job Location</label>
-                        <input type="text" name='location' placeholder='Enter your preferred area' className='w-full px-5 py-2 rounded-full border outline-none shadow-none' />
+                        <input onChange={handleInputChange} type="text" required name='location' value={formData.location} placeholder='Enter your preferred area' className='w-full px-5 py-2 rounded-full border outline-none shadow-none' />
                     </div>
                 </div>
 
                 <div className='flex flex-col gap-2 mb-2'>
                     <label>Employment Type</label>
-                    <input type="text" name='type' placeholder='Enter your type' className='w-full px-5 py-2 rounded-full border outline-none shadow-none' />
+                    <input onChange={handleInputChange} type="text" required name='type' value={formData.type} placeholder='Enter your type' className='w-full px-5 py-2 rounded-full border outline-none shadow-none' />
                 </div>
 
                 <div className="flex flex-col space-y-2 mb-4">
@@ -75,15 +111,20 @@ const ApplyJob = () => {
                     </label>
                     <input
                         id="resumeUpload"
+                        required
                         type="file"
                         name='resume'
-                        onChange={handleFileURL}
+                        onChange={(e) => {
+                            handleFileURL(e);
+                            handleInputChange(e);
+                        }}
                         accept=".pdf,.doc,.docx"
                         className="w-full border border-gray-300 rounded-lg cursor-pointer px-4 py-2 file:mr-4 file:py-2 file:px-4 file:rounded file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
                     />
+
                 </div>
                 {
-                    fileUrl && showURL &&(
+                    fileUrl && showURL && (
                         <a href={fileUrl}
                             target='_blank'
                             className="text-blue-600 underline hover:text-blue-800 transition"
@@ -95,9 +136,7 @@ const ApplyJob = () => {
 
 
                 }
-
-
-                <button onClick={() => setShowURL(false)} className='bg-blue-600 px-4 py-2 w-full text-white mt-4 rounded-full cursor-pointer'>
+                <button disabled={!verifyForm()} onClick={() => { setShowURL(false); verifyForm() }} className={`bg-blue-600 px-4 py-2 w-full text-white mt-4 rounded-full ${verifyForm() ? 'cursor-pointer' : 'opacity-50 cursor-not-allowed'}`}>
                     Submit Application
                 </button>
             </form>

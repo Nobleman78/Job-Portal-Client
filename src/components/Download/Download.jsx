3339b4e999232ec5b4a30c767d5797a47757f91d
Jesmin Chakma <jesminchakma39@gmail.com>
2025-05-24 22:25:44 +0600

    Google play store added

---
+++ src/components/Download/Download.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Download = () => {
+    return (
+        <div>
+            <h2>Download it from google play store </h2>
+        </div>
+    );
+};
+
+export default Download;
